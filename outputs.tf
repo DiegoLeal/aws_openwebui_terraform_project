c5ee9a9aad4349bb97a267ccc1ffedd8bb8fde54
DiegoLeal <diegogmach@outlook.com>
2025-05-29 13:55:00 -0300

    open-webUI-terraform

--- outputs.tf
+++ outputs.tf
@@ -12,7 +12,7 @@ output "instance_2_public_ip" {
 
 # --- Output do nome do bucket S3 ---
 
-output "s3_bucket_name_output" { # Renomeado para ser mais claro e evitar conflitos
+output "s3_bucket_name_output" { 
   description = "Nome do bucket S3 criado"
-  value       = aws_s3_bucket.open_webui_terraform_bucket.id # Referencia o novo nome do recurso
+  value       = aws_s3_bucket.open_webui_terraform_bucket.id 
 }
