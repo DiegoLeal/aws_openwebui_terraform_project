1a9b462b6516fa1212171c67dea3e353517462fe
DiegoLeal <diegogmach@outlook.com>
2025-05-29 09:58:54 -0300

    open-webUI-terraform

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,17 @@
-output "open_webui_public_ip" {
-  value = aws_instance.open_webui.public_ip
+# --- Output (Opcional, mas útil) ---
+# Para ver os IPs públicos das instâncias após o apply
+
+output "instance_1_public_ip" {
+  description = "IP público da primeira instância EC2"
+  value       = aws_instance.instance_1.public_ip
 }
 
-output "ollama_models_public_ip" {
-  value = aws_instance.ollama_models.public_ip
+output "instance_2_public_ip" {
+  description = "IP público da segunda instância EC2"
+  value       = aws_instance.instance_2.public_ip
 }
 
-output "s3_bucket_name" {
-  value = aws_s3_bucket.pdf_bucket.id
+output "s3_bucket_name_output" { # RENOMEADO para ser único e claro
+  description = "Nome do bucket S3 criado"
+  value       = aws_s3_bucket.open-webUI-terraform.id # Referenciando o novo bucket
 }
